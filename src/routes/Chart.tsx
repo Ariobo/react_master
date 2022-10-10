494b3f4a7bc69dd0b0a987904e0e5fa0a1e412ef
hajin <dhflgkwls87@gmail.com>
2022-10-11 02:57:08 +0900

    test coin end

---
+++ src/routes/Chart.tsx
@@ -0,0 +1,28 @@
+import ApexCharts from "apexcharts";
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "../api";
+
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
+
+interface ChartProps {
+  coinId: string;
+}
+function Chart({ coinId }: ChartProps) {
+  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
+    fetchCoinHistory(coinId)
+  );
+  return (
+    <h1>Chart</h1>
+  );
+}
+
+export default Chart;
